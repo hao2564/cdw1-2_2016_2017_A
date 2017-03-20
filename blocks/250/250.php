9c9d8e3a100f6a986592a8c930b20c258d6c27c0
hao2564 <nhathaonguyen96@gmail.com>
2017-03-20 13:25:38 +0700

    251
    251

--- blocks/250/250.php
+++ blocks/250/250.php
@@ -3,62 +3,52 @@
     <title></title>
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
     <?php
     if (!class_exists('lessc')) {
         include ('./libs/lessc.inc.php');
     }
     $less = new lessc;
-    $less->compileFile('less/300.less', 'css/300.css');
+    $less->compileFile('less/250.less', 'css/250.css');
     ?>
 
-    <link href="css/300.css" rel="stylesheet" type="text/css"/>
-    
+    <link href="css/250.css" rel="stylesheet" type="text/css"/>
+
 </head>
 
 <body>
-
-    <div class="type-300">
-
-        <!--FOOTER-->
-        <footer>
-            <div class="container intro">
-                <div class="row">
-                    <!--FOOTER INTRODUCTION-->
-                    <div class="col-md-4">
-                        <div class="footer-info">
-                            <h4>KHOA CÔNG NGHỆ THÔNG TIN<br>
-                                Trường Cao Đẳng Công Nghệ Thủ Đức</h4>
-                            <p>53 Võ Văn Ngân, P.Linh Chiểu, Q.Thủ Đức, Tp.HCM</p>
-                            <p>Điện thoại: (08) 22 158 642</p>
-                            <p>Fax: 08.38962474</p>
-                            <p>Email: cntt@mail.tdc.edu.vn</p>
-                        </div>
+    <div class="type-250">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="navbar-header">
+                        <small>All rights reserved 2014-by pebas</small>
                     </div>
-                    <!--/END FOOTER INTRODUCTION-->
-
-                    <!--FOOTER LINK-->
-                    <div class="col-md-4">
-                        <ul class="bottom-link">
-                            <li><a href="#">Giới thiệu</a></li>
-                            <li><a href="#">Trường Cao Đẳng Công Nghệ Thủ Đức</a></li>
-                        </ul>
-                    </div>
-                    <!--/END FOOTER LINK-->
-
-                    <!--FOOTER COPYRIGHT-->
-                    <div class="col-md-4 ">
-                        <ul class="bottom-link">
-                            <li><a href="#">Liên hệ</a></li>
-                            <li>2017 <a href="#">Khoa Công nghệ thông tin | Cao đẳng Công nghệ Thủ Đức</a> All Rights Reserved.</li>
-                        </ul>
-                    </div>
-                    <!--/END FOOTER COPYRIGHT-->
                 </div>
-            </div>
-        </footer>
-        <!--/END FOOTER-->
+                <div  class="col-md-8">
+                    <div class="navbar-menu">
+                        <nav class="navbar navbar-default collapsed bottom-nav" role="navigation">
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">					
+                                <ul class="nav navbar-nav">
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-38 current_page_item menu-item-397"><a title="Home" href="#" class=" active">Home</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-401"><a title="All Shops" href="#">All Shops</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-398"><a title="Top 20" href="#">Top 20</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-395"><a title="Daily Offers" href="#">Daily Offers</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396"><a title="Expiring" href="#">Expiring</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399"><a title="Categories" href="#">Categories</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-394"><a title="Contact" href="#">Contact</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-400"><a title="Blog" href="#">Blog</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
 
+            </div>
+        </div>
     </div>
 
 </body>
