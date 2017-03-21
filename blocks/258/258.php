8ef1a964d665657f6ba5edb46a814484aba3d311
hao2564 <nhathaonguyen96@gmail.com>
2017-03-21 11:35:52 +0700

    258
    258

---
+++ blocks/258/258.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="css/bootstrap.min.js" type="text/javascript"></script>
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/258.less', 'css/258.css');
+        ?>
+        <link href="css/258.css" rel="stylesheet" type="text/css" />
+
+    </head>
+    <body>
+        <div class="type-258">
+            <section class="blog-home">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="widget right_widget">
+                                <div class="blog-inner widget-inner">
+                                    <div class="line-divider widget-line-divider">
+                                    </div>
+                                    <div class="caption widget-caption">
+                                        <h4>Search Blog</h4>
+                                    </div>
+                                    <form role="search" method="get" id="searchform" class="searchform form-control-custom" action="http://demo.powerthemes.club/themes/couponer/">
+                                        <div>
+                                            <input type="text" value="" name="s" id="s" placeholder="Search...">
+                                            <input type="submit" id="searchsubmit" value="Search">
+                                        </div>
+                                        <input type="hidden" value="1" name="captcha">
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="widget right_widget">
+                                <div class="blog-inner widget-inner">
+                                    <div class="line-divider widget-line-divider">                   
+                                    </div>
+                                    <div class="caption widget-caption">
+                                        <h4>Archives</h4>
+                                    </div>		
+                                    <ul>
+                                        <li>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/2015/06/">June 2015</a>
+                                        </li>
+                                        <li>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/2014/06/">June 2014</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="widget right_widget">
+                                <div class="blog-inner widget-inner">
+                                    <div class="line-divider widget-line-divider">
+                                    </div>
+                                    <div class="caption widget-caption">
+                                        <h4>Coupon Categories</h4>
+                                    </div>
+                                    <ul class="list-group">
+                                        <li class="list-group-item">
+                                            <span class="badge">2</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/computers/"> Computers </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">1</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/house/"> House </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">1</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/photography/"> Photography </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">1</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/students/"> Students </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">2</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/technology/"> Technology </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">2</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/baby/"> Baby </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">1</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/books/"> Books &amp; Movies </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">2</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/cars/"> Cars </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">3</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/music/"> Music </a>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="badge">1</span>
+                                            <a href="http://demo.powerthemes.club/themes/couponer/code_category/pet-care/"> Pet Care </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>		
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
+
