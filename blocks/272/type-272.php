e20d00cc8cfcd30b22e0f1bf50c824f145ee1abd
Nhân Võ <tannhan113@gmail.com>
2017-04-04 09:36:36 +0700

    Update 272
    Update 272

--- blocks/272/type-272.php
+++ blocks/272/type-272.php
@@ -1,48 +1,48 @@
 <div class="type-272">
     <div class="container-fluid">
         <div class="row">
-            <div>
-                <div class="list-services">
-                    <div class="vc_col-sm-offset-2 vc_col-sm-8">
-                        <h3>
-                            <a href="#">ONE DAY OCEAN FISHING TRIP
-                                <small>
-                                    ETIAM HABEBIS SEM DICANTUR MADNE MOLLIS
-                                </small>
-                            </a>                      
-                        </h3>
-                        <h3>
-                            <a href="#">FOUR HOURS GUIDED TOUR
-                                <small>
-                                    PHASELLUS LAOREET LOREM VAL DOLOR TEMPUS
-                                </small>
-                            </a>
-                        </h3>
-                        <h3>
-                            <a href="#">SNORKELING
-                                <small>PAULLUM DELIQUIT, PONDERIBUS MODULISQUE
-                                </small>
-                            </a>
-                        </h3>
+           
+            <div class="list-services">
+                <div class="vc_col-sm-offset-2 vc_col-sm-8">
+                    <h3>
+                        <a href="#">ONE DAY OCEAN FISHING TRIP
+                            <small>
+                                ETIAM HABEBIS SEM DICANTUR MADNE MOLLIS
+                            </small>
+                        </a>                      
+                    </h3>
+                    <h3>
+                        <a href="#">FOUR HOURS GUIDED TOUR
+                            <small>
+                                PHASELLUS LAOREET LOREM VAL DOLOR TEMPUS
+                            </small>
+                        </a>
+                    </h3>
+                    <h3>
+                        <a href="#">SNORKELING
+                            <small>PAULLUM DELIQUIT, PONDERIBUS MODULISQUE
+                            </small>
+                        </a>
+                    </h3>
 
-                        <h3>
-                            <a href="#">TWO HOURS SCUBA DIVING
-                                <small>
-                                    VIVAMUS SAGITTIS LACUS VEL AUGUE LAOREET
-                                </small>
-                            </a>
-                        </h3>
-                    </div>
-                    <div class="clearfix"></div>
+                    <h3>
+                        <a href="#">TWO HOURS SCUBA DIVING
+                            <small>
+                                VIVAMUS SAGITTIS LACUS VEL AUGUE LAOREET
+                            </small>
+                        </a>
+                    </h3>
                 </div>
-                <div class="fs-background-container">
-                    <div class="fs-background-media  fs-background-animated" aria-hidden="true">                    
-                        <video loop="" muted="" autoplay loop>                          
-                                <source src="images/snorkelling.mp4" type="video/mp4">                             
-                        </video>                               
-                    </div>
-                </div>
+                <div class="clearfix"></div>
             </div>
+            <div class="fs-background-container">
+                                   
+                    <video loop="" muted="" autoplay loop>                          
+                            <source src="images/snorkelling.mp4" type="video/mp4">                             
+                    </video>                               
+                
+            </div>
+           
         </div>
     </div>
 </div>
