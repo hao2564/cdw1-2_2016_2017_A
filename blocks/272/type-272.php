35b984f716c2e51ef533bfeacafa85d8e7c67a17
Nhân Võ <tannhan113@gmail.com>
2017-04-04 07:24:14 +0700

    Update
    Update

--- blocks/272/type-272.php
+++ blocks/272/type-272.php
@@ -1,50 +1,47 @@
 <div class="type-272">
-    <div id="pic_1">
-        <div class="container">
-            <div id="junior_suite" class="vc_row">
-                <div class="wpb_column vc_column_container vc_col-sm-6">
-                    <div class="vc_column-inner ">
-                        <div class="wpb_wrapper">
-                            <div class="wpb_text_column wpb_content_element white-box content-padding">
-                                <div class="wpb_wrapper">
-                                    <h2 class="text-uppercase">
-                                        <small>STARTING AT $120 / NIGHT&nbsp;</small>
-                                        SIGNATURE JUNIOR SUITE
-                                    </h2>
-                                    <p>Quae vero auctorem tractata ab fiducia dicuntur. Morbi fringilla convallis sapien, id pulvinar odio volutpat</p>
-                                    <ul class="list-pointer">
-                                        <i class="fa fa-caret-right" aria-hidden="true"></i>
-                                        Cum ceteris in veneratione tui montes<br>
-                                        <i  class="fa fa-caret-right" aria-hidden="true"></i>
-                                        Fabio vel iudice vincam, sunt in culpa qui officia<br>
-                                        <i  class="fa fa-caret-right" aria-hidden="true"></i>
-                                        Gallia est omnis divisa<br>
-                                        <i  class="fa fa-caret-right" aria-hidden="true"></i>
-                                        Ut enim ad minim veniam, quis nostrud exercitation<br>
-                                        <i  class="fa fa-caret-right" aria-hidden="true"> </i>
-                                        Fabio vel iudice vincam sunt<br>
-                                    </ul>
-                                    <p>
-                                        <a class="btn btn-inline" href="#">Book The Signature Junior&nbsp;Suite Now</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div>
+                <div class="list-services">
+                    <div class="vc_col-sm-offset-2 vc_col-sm-8">
+                        <h3>
+                            <a href="#">ONE DAY OCEAN FISHING TRIP
+                                <small>
+                                    ETIAM HABEBIS SEM DICANTUR MADNE MOLLIS
+                                </small>
+                            </a>                      
+                        </h3>
+                        <h3>
+                            <a href="#">FOUR HOURS GUIDED TOUR
+                                <small>
+                                    PHASELLUS LAOREET LOREM VAL DOLOR TEMPUS
+                                </small>
+                            </a>
+                        </h3>
+                        <h3>
+                            <a href="#">SNORKELING
+                                <small>PAULLUM DELIQUIT, PONDERIBUS MODULISQUE
+                                </small>
+                            </a>
+                        </h3>
+
+                        <h3>
+                            <a href="#">TWO HOURS SCUBA DIVING
+                                <small>
+                                    VIVAMUS SAGITTIS LACUS VEL AUGUE LAOREET
+                                </small>
+                            </a>
+                        </h3>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="fs-background-container">
+                    <div class="fs-background-media  fs-background-animated" aria-hidden="true">                    
+                        <video loop="" muted="" autoplay loop>                          
+                                <source src="images/snorkelling.mp4" type="video/mp4">                             
+                        </video>                               
                     </div>
                 </div>
-
-                <div class="wpb_column vc_column_container vc_col-sm-6">
-                    <div class="vc_column-inner ">           
-                        <div class="wpb_single_image wpb_content_element vc_align_left">
-                            <figure class="wpb_wrapper vc_figure">
-                                <div class="vc_single_image-img vc_box_border_grey">
-                                    <img  src="images/content-room.jpg">
-                                </div>
-                            </figure>
-                        </div>              
-                    </div>
-                </div>
-
             </div>
         </div>
     </div>
