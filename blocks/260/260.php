9ad771b5846c4260c01488746b96a62ac0eef333
nhutran96 <tranvannhu96@gmail.com>
2017-04-13 08:24:40 +0700

    fix sliveshow
    fix

--- blocks/260/260.php
+++ blocks/260/260.php
@@ -6,10 +6,12 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <script src="css/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/swiper.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/swiper.min.js" type="text/javascript"></script>
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
@@ -23,66 +25,35 @@
     </head>
     <body>
         <div class="type-1">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="carousel slide" data-ride="carousel" id="quote-carousel">
-                            <!-- Carousel Slides / Quotes -->
-                            <div class="carousel-inner">
-                                <!-- Quote 1 -->
-                                <div class="item active">
-                                    <blockquote>
-                                        <div class="row">
-                                            <div class="col-md-12 col-sm-12 col-xs-12">
-                                                <div class="thumb-img">
-                                                    <img src="images/a.jpg" alt=""/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </blockquote>
-                                </div>
-                                <!-- Quote 2 -->
-                                <div class="item">
-                                    <blockquote>
-                                        <div class="row">
-                                            <div class="col-md-12 col-sm-12 colxs-12">
-                                                <div class="thumb-img">
-                                                    <img src="images/b.jpg" alt=""/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </blockquote>
-                                </div>
-                                <!-- Quote 3 -->
-                                <div class="item">
-                                    <blockquote>
-                                        <div class="row">
-                                            <div class="col-md-12 col-sm-12 col-xs-12">
-                                                <div class="thumb-img">
-                                                    <img src="images/shopping1.jpg" alt=""/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </blockquote>
-                                </div>
-                            </div>
-
-                            <!-- Carousel Buttons Next/Prev -->
-                            <a data-slide="prev" href="#quote-carousel" class="left carousel-control"><i class="fa fa-chevron-left"></i></a>
-                            <a data-slide="next" href="#quote-carousel" class="right carousel-control"><i class="fa fa-chevron-right"></i></a>
-                        </div>                          
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="images/a.jpg" alt=""/>
                     </div>
+                    <div class="swiper-slide">
+                        <img src="images/b.jpg" alt=""/>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/shopping1.jpg" alt=""/>
+                    </div>
+
                 </div>
+                <div class="swiper-button-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+                <div class="swiper-button-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
             </div>
+
         </div>
     </body>
 </html>
+
 <script>
-    $(document).ready(function () {
-        //Set the carousel options
-        $('#quote-carousel').carousel({
-            pause: true,
-            interval: 4000,
-        });
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
     });
 </script>
