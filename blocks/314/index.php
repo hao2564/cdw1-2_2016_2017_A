97c7b4ce8578a54fb49a75c7205e8133729c3648
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:13:48 +0700

    Add module 314
    Add module 314

---
+++ blocks/314/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php if (!class_exists( 'lessc')) { include ( './libs/lessc.inc.php'); }
+        $less=new lessc;
+        $less->compileFile('less/type-314.less', 'css/type-314.css');
+        ?>
+        <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+        <link href="css/type-314.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <title>type-314</title>
+    </head>
+
+    <body>
+      
+        <?php
+        include 'type-314.php';
+        ?>
+        
+        <script>
+           
+            
+            function toggleIcon(e) {
+                $(e.target)
+                    .prev('.panel-heading')
+                    .find(".more-less")
+                    .toggleClass('glyphicon-plus glyphicon-minus');
+            }
+            $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+            $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+        </script>
+     
+    </body>
+
+</html>
+
+
+
+
---
+++ blocks/314/type-314.php
@@ -0,0 +1,268 @@
+<div class="type-314">
+    <div class="container">
+        <div class="row">             
+            <div class="col-md-8 col-xs-12 col-sm-8 col-lg-8">
+                <!--------BUTTON1--------------------->
+                <h3>Small Buttons</h3>
+                <div class="head1">
+                    <button type="button" class="btn btn-success">CLICK ME</button>
+                    <button type="button" class="btn btn-warning">CLICK ME</button>
+                    <button type="button" class="btn btn-success btn-throat">CLICK ME</button>
+                    <button type="button" class="btn btn-danger">CLICK ME</button>
+                    <button type="button" class="btn btn-info">CLICK ME</button>
+
+                    <div class="clearfix"></div>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingOne">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                <div class="panel-body">
+                                    [gdlr_button href="http://www.goodlayers.com" target="_self" size="small" background="#94d64f" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="small" background="#d6724f" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="small" background="#d64f78" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="small" background="#d64f4f" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="small" background="#4fbed6" color="#ffffff"]Click Me[/gdlr_button]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!--------BUTTON2--------------------->
+                <h3>Medium Buttons</h3>
+                <div class="head1">
+                    <button type="button" class="btn btn-success success1">CLICK ME</button>
+                    <button type="button" class="btn btn-warning warning1">CLICK ME</button>
+                    <button type="button" class="btn btn-info info1">CLICK ME</button>
+                    <button type="button" class="btn btn-danger throat1">CLICK ME</button>
+                    <button type="button" class="btn btn-primary">CLICK ME</button>
+
+                    <div class="clearfix"></div>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingTwo">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                <div class="panel-body">
+                                    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#94d64f" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#d6724f" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#4fbed6" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#d64f92" color="#ffffff"]Click Me[/gdlr_button]<br>
+    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#4f8ed6" color="#ffffff"]Click Me[/gdlr_button]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!--------BUTTON3--------------------->
+                <h3>Large Buttons</h3>
+                <div class="head1">
+                    <button type="button" class="btn btn-success success2">CLICK ME</button>
+                      <button type="button" class="btn btn-danger throat2">CLICK ME</button>
+                    <button type="button" class="btn btn-warning warning2">CLICK ME</button>
+                    <button type="button" class="btn btn-info info2">CLICK ME</button>
+                  
+                    <button type="button" class="btn btn-danger danger2">CLICK ME</button>
+
+                    <div class="clearfix"></div>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingThree">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                                <div class="panel-body">
+                                    [gdlr_button href="http://www.goodlayers.com" target="_self" size="large" background="#94d64f" color="#ffffff"]Click Me[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="large" background="#d64f78" color="#ffffff"]Click Me[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="large" background="#d6724f" color="#ffffff"]Click Me[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="large" background="#4fbed6" color="#ffffff"]Click Me[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="large" background="#d64f4f" color="#ffffff"]Click Me[/gdlr_button]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--------BUTTON4--------------------->
+                <h3>Large Buttons</h3>
+                <div class="head1">                  
+                    <button type="button" class="btn btn-success success3 btn1">BUTTON TEXT</button>                  
+                    <button type="button" class="btn btn-warning warning3 btn2">BUTTON TEXT</button>
+                    <button type="button" class="btn btn-info info3 btn3">BUTTON TEXT</button>                                  
+                    <div class="clearfix"></div>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFour">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+                                <div class="panel-body">
+                                    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#94d64f" color="#ffffff" with_border="yes" border_color="#5c8f26"]Button Text[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#d6724f" color="#ffffff" with_border="yes" border_color="#98543c"]Button Text[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#4fbed6" color="#ffffff" with_border="yes" border_color="#368799"]Button Text[/gdlr_button]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                 <!--------BUTTON5--------------------->
+                <h3>Medium Buttons With Border & Icon</h3>
+                <div class="head1">                  
+                    <button type="button" class="btn btn-success success3 btn1">[gdlr_icon type="icon-headphones"] Button Text</button>                  
+                    <button type="button" class="btn btn-warning warning3 btn2">[gdlr_icon type="icon-plane"] Button Text</button>
+                    <button type="button" class="btn btn-info info3 btn3">[gdlr_icon type="icon-apple"] Button Text</button>                                  
+                    <div class="clearfix"></div>
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingFive">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+                                <div class="panel-body">
+                                    [gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#94d64f"<br> color="#ffffff" with_border="yes" border_color="#5c8f26"][gdlr_icon type="icon-headphones"] Button Text[/gdlr_button]<br>
+[gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#d6724f"<br> color="#ffffff" with_border="yes" border_color="#98543c"][gdlr_icon type="icon-plane"] Button <br>Text[/gdlr_button]
+<br>[gdlr_button href="http://www.goodlayers.com" target="_self" size="medium" background="#4fbed6"<br> color="#ffffff" with_border="yes" border_color="#368799"][gdlr_icon type="icon-apple"] Button<br> Text[/gdlr_button]
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-xs-12 col-sm-4 col-lg-4">
+                <div class="title">                 
+                    <h4><i class="fa fa-pause" aria-hidden="true"></i>SHORTCODES</h4>
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Accordion/Toggle Box</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Audio Shortcode</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Button</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Column Shortcode</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Divider</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Gallery Shortcode</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Heading Tag</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">icons & icon boxes</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Lightbox & Frames</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Notification Boxes</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Personnel</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Price Table</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Process</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Process</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Skills</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Slider Shortcode</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Space</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Space</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Stunning Text</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Styled Box</a>                   
+                </div>
+                 <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Table</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Tabs</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Testimonial & Quote</a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Typography/a>                   
+                </div>
+                <div class="content">
+                    <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    <a href="#">Video</a>                   
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
