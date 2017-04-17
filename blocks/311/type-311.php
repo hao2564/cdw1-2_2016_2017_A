f74cf9b129d74fde0425aa4a8f98874ad1a9b13a
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:11:39 +0700

    Add Module  311
    Add Module  311

---
+++ blocks/311/type-311.php
@@ -0,0 +1,367 @@
+<div class="type-311">
+    <div class="container">
+        <div class="row"> 
+            <!-- HINH 1-->
+              <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/1.jpg" alt="">
+                    </a>
+                    <div class="iconimage">
+                        <i class="icon-bullhorn fa fa-bullhorn"></i>Sticky Post
+                    </div>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">Sedial eiusmod tempor</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Dec 2013</a>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+            <!--- HINH 2----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/2.jpg" alt="">
+                    </a>                   		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">DONEC LUCTUS IMPERDIET</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Dec 2013</a>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Blog, Link, News</a>
+                        </span>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+            <!--- HINH 3----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/4.jpg" alt="">
+                    </a>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">MAGNA PARS STUDIORUM</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Dec 2013</a>
+                    </div>
+                    <div class="blog-info blog-3">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Animal, Safari</a>
+                        </span>
+                    </div>
+                    <div class="blog-info ">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>          
+            <!--- HINH 4----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/5.jpg" alt="">
+                    </a>                   		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">EIUSMOD TEMPOR INCIDUNT</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Dec 2013</a>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Blog, Life Style</a>
+                        </span>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+            <!--- HINH 5----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/6.jpg" alt="">
+                    </a>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">STANDARD POST FORMAT TITLE</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">05 Oct 2013</a>
+                    </div>
+                    <div class="blog-info blog-3">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Blog, Life Style</a>
+                        </span>
+                    </div>
+                    <div class="blog-info ">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+            <!--- HINH 6----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/7.jpg" alt="">
+                    </a>                   		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">DIEM CERTAM INDICERE</a>
+                    </h3>
+                    <div class="blog-info blog-6">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Oct 2013</a>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Blog, Business, identity, Life Style</a>
+                        </span>
+                    </div>
+                    <div class="blog-info">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+            <!--- HINH 7----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/8.jpg" alt="">
+                    </a>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">PARTURIENT MONTES</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Step 2013</a>
+                    </div>
+                    <div class="blog-info blog-3">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">identity, Life Style</a>
+                        </span>
+                    </div>
+                    <div class="blog-info ">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+             <!--- HINH 8----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/9.jpg" alt="">
+                    </a>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">NIHILNE TE NOCTURNU</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">03 Aug 2013</a>
+                    </div>
+                    <div class="blog-info blog-3">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Audio, Business, Life Style</a>
+                        </span>
+                    </div>
+                    <div class="blog-info ">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+              <!--- HINH 8----------->
+            <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4 margin">               
+                <div class="image">
+                    <a href="#"> 
+                        <img src="images/10.jpg" alt="">
+                    </a>		
+                </div>
+
+                <div class="post-header">
+                    <h3 class="title ">
+                        <a href="#">DIEM CERTAM INDICERE</a>
+                    </h3>
+                    <div class="blog-info">
+                        <i class="icon-calendar fa fa-calendar"></i>
+                        <a href="#">02 Aug 2013</a>
+                    </div>
+                    <div class="blog-info blog-3">
+                        <i class="icon-tag fa fa-tag"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">Business, Life Style, Travel</a>
+                        </span>
+                    </div>
+                    <div class="blog-info ">
+                        <i class="icon-user fa fa-user"></i>
+                        <span class="fn">
+                            <a href="#" title="Posts by John Smith" rel="author">John Smith</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>                                                                         
+                </div>
+
+                <div class="content">Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita quaerimus....                   
+                    <br><a href="#" class="readmore">Read More</a>
+                    <div class="blog-icon">
+                        <i class="icon-comments fa fa-comments"></i>
+                        <a href="#">0</a>
+                    </div>
+                </div>	
+             
+            </div>
+           
+           
+           
+            
+        </div>
+    </div>
+</div>
