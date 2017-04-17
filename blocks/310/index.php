37e02d148235e5371694f595ac451e8c2a2168a1
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:10:47 +0700

    Add Module 310
    Add Module 310

---
+++ blocks/310/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php if (!class_exists( 'lessc')) { include ( './libs/lessc.inc.php'); }
+        $less=new lessc;
+        $less->compileFile('less/type-310.less', 'css/type-310.css');
+        ?>
+        <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+        <script src="js/scripts.js" type="text/javascript"></script>
+        <link href="css/type-310.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <title>type-310</title>
+    </head>
+
+    <body>
+      
+        <?php
+        include 'type-310.php';
+        ?>
+        
+        <script>
+            $(document).ready(function(e) {
+            $('.thumb').click(function(e){
+                            var hinhnho = $(this).attr('src');
+                            $('#main-photo').attr('src',hinhnho);
+                    });
+            });
+            
+            function toggleIcon(e) {
+                $(e.target)
+                    .prev('.panel-heading')
+                    .find(".more-less")
+                    .toggleClass('glyphicon-plus glyphicon-minus');
+            }
+            $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+            $('.panel-group').on('shown.bs.collapse', toggleIcon);
+
+
+        </script>
+     
+    </body>
+
+</html>
+
+
+
+
---
+++ blocks/310/type-310.php
@@ -0,0 +1,41 @@
+<div class="type-310">
+    <div class="container">
+        <div class="row">  
+            
+            <div class="col-md-8 col-lg-8 col-xs-12 col-sm-8 center">
+                <div class="title">
+                    <h3>Gallery Thumbnail Style</h3>
+                </div>
+                <img src="images/m1.jpg" id="main-photo"><br> 
+                <img src="images/m1.jpg" class="thumb size1">
+                <img src="images/m2.jpg" class="thumb size">
+                <img src="images/m3.jpg" class="thumb size">
+                <img src="images/m4.jpg" class="thumb size">
+                <img src="images/m5.jpg" class="thumb size">
+                <img src="images/m6.jpg" class="thumb size">
+                <img src="images/m7.jpg" class="thumb size">
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingOne">
+                            <h4 class="panel-title">
+                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <i class="more-less glyphicon glyphicon-plus"></i>
+                                    Get the code
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                            <div class="panel-body">
+                                 [gallery columns="7" link="file" caption="yes" thumbnail_size="thumbnail" 
+                                 type="thumbnail" ids="3132,3131,3158,3113,3100,3099,3095"]
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+           
+            
+        </div>
+    </div>
+</div>
