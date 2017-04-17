edc759862ed57fe779ad65a35af8b5c6c83fe5cd
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:12:31 +0700

    Add module 312
    Add module 312

---
+++ blocks/312/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">     
+        <?php if (!class_exists( 'lessc')) { include ( './libs/lessc.inc.php'); }
+        $less=new lessc;
+        $less->compileFile('less/type-312.less', 'css/type-312.css');
+        ?>
+        <link href="css/type-312.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>        
+
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+      
+        <?php
+            include 'type-312.php';
+        ?>
+        <script>
+         $(document).ready(function () {
+                $('#owl-demo').owlCarousel({
+                    loop: true,
+                    autoPlay: 3000,
+                    nav: true,
+                    navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+                    responsive: {
+                        0: {
+                            items: 1
+                        },
+                        600: {
+                            items: 2
+                        },
+                        1000:{
+                            items: 3
+                        }
+                    }
+                })
+            });
+        </script>
+      
+     
+    </body>
+
+</html>
+
+
+
+
---
+++ blocks/312/type-312.php
@@ -0,0 +1,157 @@
+<div class="type-312">
+    <div class="container">
+        <div class="row">
+            <div class="title">PERSONNEL PLAIN STYLE WITH CAROUSEL</div>
+             
+             <div id="owl-demo" class="owl-carousel owl-theme"> 
+                
+                    <div class="info">
+                        <div class="item col-item">
+                            <div class="photo">
+                                <img src="images/1.jpg" class="img-responsive" alt="a" />
+                            </div>
+                            <div class="content-info">
+                                <div class="personnel-info">
+                                    <h3>John Superdoe</h3>
+                                    <p>Web Designer</p>
+                                </div>                                                                          
+                                <div class="content">
+                                   <p>
+                                       Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac,
+                                       vestibulum at eros. Donec ullamcorper nulla non metus auctor.
+                                   </p>
+                                   <p>
+                                       <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                       <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                                       <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                       <i class="fa fa-skype" aria-hidden="true"></i>
+                                       <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                   </p>
+                                </div>
+                                <div class="clearfix">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div class="item col-item">
+                            <div class="photo">
+                                <img src="images/2.jpg" class="img-responsive" alt="a" />
+                            </div>
+                            <div class="content-info">
+                                <div class="personnel-info">
+                                    <h3>John Superdoe</h3>
+                                    <p>Web Designer</p>
+                                </div>                                                                          
+                                <div class="content">
+                                   <p>
+                                       Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac,
+                                       vestibulum at eros. Donec ullamcorper nulla non metus auctor.
+                                   </p>
+                                   <p>
+                                       <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                       <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                                       <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                       <i class="fa fa-skype" aria-hidden="true"></i>
+                                       <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                   </p>
+                                </div>
+                                <div class="clearfix">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info">
+                       <div class="item col-item">
+                           <div class="photo">
+                               <img src="images/3.jpg" class="img-responsive" alt="a" />
+                           </div>
+                           <div class="content-info">
+                               <div class="personnel-info">
+                                   <h3>John Superdoe</h3>
+                                   <p>Web Designer</p>
+                               </div>                                                                          
+                               <div class="content">
+                                  <p>
+                                      Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac,
+                                      vestibulum at eros. Donec ullamcorper nulla non metus auctor.
+                                  </p>
+                                  <p>
+                                      <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                      <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                                      <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                      <i class="fa fa-skype" aria-hidden="true"></i>
+                                      <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                  </p>
+                               </div>
+                               <div class="clearfix">
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                    <div class="info">
+                       <div class="item col-item">
+                           <div class="photo">
+                               <img src="images/4.jpg" class="img-responsive" alt="a" />
+                           </div>
+                           <div class="content-info">
+                               <div class="personnel-info">
+                                   <h3>John Superdoe</h3>
+                                   <p>Web Designer</p>
+                               </div>                                                                          
+                               <div class="content">
+                                  <p>
+                                      Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac,
+                                      vestibulum at eros. Donec ullamcorper nulla non metus auctor.
+                                  </p>
+                                  <p>
+                                      <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                      <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                                      <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                      <i class="fa fa-skype" aria-hidden="true"></i>
+                                      <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                  </p>
+                               </div>
+                               <div class="clearfix">
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                    <div class="info">
+                        <div class="item col-item">
+                            <div class="photo">
+                                <img src="images/5.jpg" class="img-responsive" alt="a" />
+                            </div>
+                            <div class="content-info">
+                                <div class="personnel-info">
+                                    <h3>John Superdoe</h3>
+                                    <p>Web Designer</p>
+                                </div>                                                                          
+                                <div class="content">
+                                   <p>
+                                       Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac,
+                                       vestibulum at eros. Donec ullamcorper nulla non metus auctor.
+                                   </p>
+                                   <p>
+                                       <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                       <i class="fa fa-twitter-square" aria-hidden="true"></i>
+                                       <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                                       <i class="fa fa-skype" aria-hidden="true"></i>
+                                       <i class="fa fa-dribbble" aria-hidden="true"></i>
+                                   </p>
+                                </div>
+                                <div class="clearfix">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 
+                 
+                 
+                 
+                    </div>
+                </div>
+        </div>
+     
+    </div>
+</div>
