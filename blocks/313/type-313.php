5bad2c481edd5486b78f86d4440eb8330377f5b3
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:13:11 +0700

    Add module 313
    Add module 313

---
+++ blocks/313/type-313.php
@@ -0,0 +1,134 @@
+<div class="type-313">
+    <div class="container">      
+        <div class="row" style="margin-top:30px;">
+            <h2>Lightbox and Frames</h2>
+            <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6 margin">              
+                <div class="demo">
+                    <a href="#">
+                        <img class="" src="images/1.jpg">
+                        <div class="overlay thumb"></div>
+                        <div class="text thumb">
+                            <i class="fa fa-link icon" aria-hidden="true"></i>
+                        </div>
+
+                    </a>
+                </div>
+                <p>Normal Style, Link To URL</p>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6 margin">              
+                <div class="demo nhan-1">
+                    <a href="#">
+                        <img class="" src="images/1.jpg">
+                        <div class="overlay thumb"></div>
+                        <div class="text thumb">
+                            <i class="fa fa-search icon" aria-hidden="true"></i>
+                        </div>
+                        <div class="clearfix"></div>
+                    </a>
+                </div>
+                <p>Normal Style, Link To URL</p>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6 margin">              
+                <div class="demo border ">
+                    <a href="#">
+                        <img class="" src="images/1.jpg">
+                        <div class="overlay thumb"></div>
+                        <div class="text thumb">
+                            <i class="fa fa-film icon" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <p>Normal Style, Link To URL</p>
+            </div>
+            <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6 margin">              
+                <div class="demo thumb nhan-3">
+                    <a href="#">
+                        <img class="radius" src="images/1.jpg">
+                        <div class="overlay"></div>
+                        <div class="text thumb">
+                            <i class="fa fa-search icon" aria-hidden="true"></i>
+                        </div>
+
+                    </a>
+                </div>
+
+                <p>Normal Style, Link To URL</p>
+            </div>
+           
+            <div class="clearfix"></div>
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingOne">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <i class="more-less glyphicon glyphicon-plus"></i>
+                                Get the code
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                            [gdlr_frame type="normal" align="left" caption="Normal Style, Link To URL"] [gdlr_image_link type="url" image_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" alt=" " link_url="http://goodlayers.com" target="_blank" width="310px"][/gdlr_frame]
+                            [gdlr_frame type="border" align="left" caption="Border Style, Open image With Lightbox"] [gdlr_image_link type="image" image_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" alt=" " link_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" target="_blank" width="310px"][/gdlr_frame]
+                            [gdlr_frame type="solid" align="left" caption="Solid Style, Open Video With Lightbox" background_color="#aa4243"] [gdlr_image_link type="video" image_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" alt=" " link_url="http://www.youtube.com/watch?v=SZEflIVnhH8" width="310px"][/gdlr_frame]
+                            [gdlr_frame type="rounded" align="left" caption="Rounded Corner, Open image With Lightbox"] [gdlr_image_link type="image" image_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" link_url="http://demo.goodlayers.com/versatile/wp-content/uploads/2013/11/photodune-5347441-the-brooklyn-bridge-in-new-york-city-m.jpg" alt=" " width="310px"]
+                            [/gdlr_frame]
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <h3>How About Circle Frame?</h3>
+            <div class="col-lg-12 col-sm-12 col-xs-12 col-md-12 margin">              
+                <div class="demo nhan-2">
+                    <a href="#">
+                        <img class="tron" src="images/2.jpg">
+                        <div class="overlay thumb"></div>
+                        <div class="text thumb">
+                            <i class="fa fa-search icon" aria-hidden="true"></i>
+                        </div>
+
+                    </a>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingTwo">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                <i class="more-less glyphicon glyphicon-plus"></i>
+                                Get the code
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                        <div class="panel-body">
+                            [gdlr_frame type="circle border" background="#aa4243" align="center"] [gdlr_image_link type="image"
+                            image_url="http://demo.goodlayers.com/flawless/wp-content/uploads/2013/11/phone.jpg" alt=" " 
+                            link_url="http://demo.goodlayers.com/flawless/wp-content/uploads/2013/11/phone.jpg" target="_blank" width="350px"][/gdlr_frame]
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+    <div tabindex="-1" class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button class="close" type="button" data-dismiss="modal">
+                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                    </button>                 
+                </div>
+                <div class="modal-body">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</div>
