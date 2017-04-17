293d7200f6a7bc7b59f335af2dff1ea0e722d3db
Nhân Võ <tannhan113@gmail.com>
2017-04-17 14:15:23 +0700

    Add module 317
    Add module 317

---
+++ blocks/317/type-317.php
@@ -0,0 +1,88 @@
+<div class="type-317">
+    <div class="container">
+        <div class="row">  
+            <div class="col-md-8 col-xs-12 col-lg-8 col-sm-8 margin">
+                
+                <!-- Indicators -->
+                <h2>Post Slider With Caption At The Bottom</h2>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Wrapper for slides -->             
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="images/4.jpg" alt="Chania">
+                            <div class="carousel-caption">
+                                <h3>Diem certam indicere</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna<br>
+                                    aliqua.Idque Caesaris facere voluntate liceret: sese habere....</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/1.jpg" alt="Chania">
+                            <div class="carousel-caption">
+                                <h3>Nihilne te nocturnu</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere....</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/2.jpg" alt="Flower">
+                            <div class="carousel-caption">
+                                <h3>Parturient montes</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere....</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="images/3.jpg" alt="Flower">
+                            <div class="carousel-caption">
+                                <h3>Diem certam indicere</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Idque Caesaris facere voluntate liceret: sese habere....</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Left and right controls -->
+                    <div class="button">
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                           
+                            <span class="fa fa-angle-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                             
+                            <span class="fa fa-angle-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                    </ol>
+                </div> 
+                
+                
+                <div class="clearfix"></div>
+               
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingOne">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <i class="more-less glyphicon glyphicon-plus"></i>
+                                        Get the code
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                <div class="panel-body">
+                                    [gdlr_post_slider category="19" num_fetch="4" num_excerpt="25" caption_position="bottom" <br>
+                                    thumbnail_size="post-slider-bottom" orderby="date" order="asc" ]
+                                </div>
+                            </div>
+                       </div>
+                 </div>
+            </div>
+           
+            
+        </div>
+    </div>
+</div>
