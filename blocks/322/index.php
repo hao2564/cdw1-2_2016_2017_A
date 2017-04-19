2e421d1dd8c564a207805f60ddcd4566d4ff638b
Nhân Võ <tannhan113@gmail.com>
2017-04-19 13:19:12 +0700

    Add Module 322
    Add Module 322

---
+++ blocks/322/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php if (!class_exists( 'lessc')) { include ( './libs/lessc.inc.php'); }
+        $less=new lessc;
+        $less->compileFile('less/type-322.less', 'css/type-322.css');
+        ?>
+        <link href="css/type-322.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <title>type-322</title>
+    </head>
+
+    <body>
+      
+        
+        <?php
+            include 'type-322.php';
+        ?>
+        
+        
+     
+    </body>
+
+</html>
+
+
+
+
---
+++ blocks/322/type-322.php
@@ -0,0 +1,56 @@
+<div class="type-322">
+    
+    <div class="background">
+        <div class="container">
+            <div class="row">   
+                <div class="col-md-4 col-sm-6 col-lg-4 col-xs-12 margin">                  
+                    <div class="content">
+                        <div class="circle-icon">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>                      
+                        </div>
+                        <h4 class="title">Contact By Email</h4>
+                        <div class="caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore 
+                                et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-4 col-xs-12 margin">                  
+                    <div class="content">
+                        <div class="circle-icon">
+                            <i class="fa fa-phone-square" aria-hidden="true"></i>                     
+                        </div>
+                        <h4 class="title">Contact By Email</h4>
+                        <div class="caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore 
+                                et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-4 col-xs-12 margin">                  
+                    <div class="content">
+                        <div class="circle-icon">
+                            <i class="fa fa-home" aria-hidden="true"></i>                     
+                        </div>
+                        <h4 class="title">Contact By Email</h4>
+                        <div class="caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore 
+                                et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    
+    
+    
+</div>
