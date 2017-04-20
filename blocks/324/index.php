f61bbdeefbf2e904a941f0e8297975c5d3bd41df
Nhân Võ <tannhan113@gmail.com>
2017-04-20 09:41:21 +0700

    Add Module 324
    Add Module 324

---
+++ blocks/324/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <?php if (!class_exists( 'lessc')) { include ( './libs/lessc.inc.php'); }
+        $less=new lessc;
+        $less->compileFile('less/type-324.less', 'css/type-324.css');
+        ?>
+        <link href="css/type-324.css" rel="stylesheet" type="text/css" /> 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+       
+        <title>type-324</title>
+    </head>
+
+    <body>
+      
+        <?php
+            include 'type-324.php';
+        ?>
+        
+        <script>
+            function toggleIcon(e) {
+                $(e.target)
+                    .prev('.panel-heading')
+                    .find(".more-less")
+                    .toggleClass('glyphicon-plus glyphicon-minus');
+            }
+            $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+            $('.panel-group').on('shown.bs.collapse', toggleIcon);
+            
+            
+            
+              
+              
+        </script>
+     
+    </body>
+
+</html>
+
+
+
+
---
+++ blocks/324/type-324.php
@@ -0,0 +1,154 @@
+<div class="type-324">
+    <div class="container">
+        <div class="row">  
+            
+            <div class="col-md-4 col-sm-4 col-lg-4 col-xs-12 margin">                  
+                <div class="content">
+                    <div class="circle-icon">
+                        <i class="fa fa-cogs" aria-hidden="true"></i>                     
+                    </div>
+                    <h4 class="title">icon on the top</h4>
+                    <div class="caption">
+                        <p>
+                           Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in,
+                           egestas eget quam. Aenean eu leo quam. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4 col-sm-4 col-lg-4 col-xs-12 margin">                  
+                <div class="content">
+                    <div class="circle-icon1">
+                       <i class="fa fa-coffee" aria-hidden="true"></i>                    
+                    </div>
+                    <h4 class="title">icon on the top</h4>
+                    <div class="caption">
+                        <p>
+                           Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in,
+                           egestas eget quam. Aenean eu leo quam. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4 col-sm-4 col-lg-4 col-xs-12 margin">                  
+                <div class="content">
+                    <div class="circle-icon2">
+                        <i class="fa fa-dropbox" aria-hidden="true"></i>                  
+                    </div>
+                    <h4 class="title">icon on the top</h4>
+                    <div class="caption">
+                        <p>
+                           Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in,
+                           egestas eget quam. Aenean eu leo quam. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 margin">                  
+                <div class="content1">
+                    <div class="circle-icon3">
+                        <i class="fa fa-flag" aria-hidden="true"></i>                 
+                    </div>
+                    <h4 class="title">icon on the top</h4>
+                    <div class="caption">
+                        <p>
+                           Curabitur blandit tempus porjusto odio, dapibus ac facilisis in, egestas eget quam.
+                           Aenen eu leo quam. Pellentesque otsem lacinia quam venenatis vestibulum.Donec id elit
+                           non mi porta gravida aeget. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-6 col-sm-6 col-lg-6 col-xs-12 margin">                  
+                <div class="content1">
+                    <div class="circle-icon4">
+                        <i class="fa fa-cloud" aria-hidden="true"></i>                  
+                    </div>
+                    <h4 class="title">icon on the top</h4>
+                    <div class="caption">
+                        <p>
+                           Curabitur blandit tempus porjusto odio, dapibus ac facilisis in, egestas eget quam. Aenen eu leo quam.
+                           Pellentesque otsem lacinia quam venenatis vestibulum.Donec id elit non mi porta gravida aeget.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="clearfix"></div>
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingOne">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <i class="more-less glyphicon glyphicon-plus"></i>
+                                Get the code
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                            [gdlr_row]<br><br>
+
+                            [gdlr_column size="1/3"]<br><br>
+
+                            [gdlr_box_icon icon="icon-gears" icon_type="circle" icon_color="#ffffff" icon_background="#d55949" 
+                            icon_position="top" title="icon on the top" ]Curabitur blandit tempus<br> porttitor. Cras justo odio, dapibus ac facilisis in,
+                            egestas eget quam. Aenean eu leo quam. [/gdlr_box_icon]<br><br>
+
+                            [/gdlr_column]<br><br>
+
+                            [gdlr_column size="1/3"]<br><br>
+
+                            [gdlr_box_icon icon="icon-coffee" icon_type="circle" icon_color="#ffffff" 
+                            icon_position="top" title="icon on the top" ]Curabitur blandit tempus porttitor. Cras justo odio,<br> 
+                            dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. [/gdlr_box_icon]<br><br>
+
+                            [/gdlr_column]<br><br>
+
+                            [gdlr_column size="1/3"]<br><br>
+
+                            [gdlr_box_icon icon="icon-dropbox" icon_type="circle" icon_color="#ffffff" icon_background="#4984d5" 
+                            icon_position="top" title="icon on the top" ]Curabitur blandit tempus<br> porttitor. Cras justo odio,
+                            dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. [/gdlr_box_icon]<br><br>
+
+                            [/gdlr_column]<br><br>
+
+                            [/gdlr_row]<br>
+                            [gdlr_space height="10px"]<br>
+                            [gdlr_row]<br><br>
+
+                            [gdlr_column size="1/2"]<br><br>
+
+                            [gdlr_box_icon icon="icon-flag-alt" icon_type="circle" icon_color="#ffffff" icon_background="#6049d5" 
+                            icon_position="left" title="icon on the top" ]Curabitur blandit tempus<br> porjusto odio, dapibus ac 
+                            facilisis in, egestas eget quam. Aenen eu leo quam. Pellentesque otsem lacinia quam venenatis vestibulum.
+                            Donec id elit non mi porta gravida aeget.<br> [/gdlr_box_icon]<br><br>
+
+                            [/gdlr_column]<br><br>
+
+                            [gdlr_column size="1/2"]<br><br>
+
+                            [gdlr_box_icon icon="icon-cloud" icon_type="circle" icon_color="#ffffff" icon_background="#49d5c0" 
+                            icon_position="left" title="icon on the top" ]Curabitur blandit tempus <br>porjusto odio, dapibus ac 
+                            facilisis in, egestas eget quam. Aenen eu leo quam. Pellentesque otsem lacinia quam venenatis vestibulum.
+                            Donec id elit non mi porta gravida aeget.<br>[/gdlr_box_icon]<br><br>
+
+                            [/gdlr_column]<br><br>
+
+                            [/gdlr_row]
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+             
+           
+            
+        </div>
+    </div>
+</div>
