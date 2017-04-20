18ec8f0aea998b0b4c6b93420777c3131d80d96e
nhutran96 <tranvannhu96@gmail.com>
2017-04-20 10:05:44 +0700

    Add module 309

---
+++ blocks/309/309.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
+        <script src="css/bootstrap.min.js" type="text/javascript"></script>
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/309.less', 'css/309.css');
+        ?>
+        <link href="css/309.css" rel="stylesheet" type="text/css" />
+
+
+    </head>
+
+    <body>
+        <div class="type-309">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-xs-12">
+                        <div class="only-icon">
+                            <h3>Only icon</h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-fire" aria-hidden="true"></i>
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                            <i class="fa fa-bell-o" aria-hidden="true"></i>
+                            <i class="fa fa-bolt" aria-hidden="true"></i>
+                            <i class="fa fa-flask" aria-hidden="true"></i>
+                            <i class="fa fa-bolt" aria-hidden="true"></i>
+                            <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                            <i class="fa fa-html5" aria-hidden="true"></i>
+                            <i class="fa fa-gamepad" aria-hidden="true"></i>
+                            <i class="fa fa-globe" aria-hidden="true"></i>
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            <i class="fa fa-group" aria-hidden="true"></i>
+                            <i class="fa fa-gear" aria-hidden="true"></i>
+                            <i class="fa fa-dollar" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-truck" aria-hidden="true"></i>
+                        </div>
+                        <!-----get the code--------->
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title">
+                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            <i class="more-less glyphicon glyphicon-plus"></i>
+                                            Get the code
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                    <div class="panel-body">
+                                        <p>[gdlr_icon type="icon-fire" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-instagram" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-figther-jet" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-bell" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-bolt" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-beaker" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-hand-right" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-html5" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-gamepad" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-globe" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-heart" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-group" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-gears" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-dollar" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-star" size="36px" color="#333333"]<br>
+                                            [gdlr_icon type="icon-truck" size="36px" color="#333333"]</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <!---------//------------>
+                        <div class="btn-list">
+                            <a href="#">List Of All Icons Can Be Viewed Here</a>
+                        </div>
+                        <!------Colored Icon------->
+                        <div class="colored-icon">
+                            <h3>Colored icon</h3>
+                        </div>
+                        <div class="icon-color">
+                            <i class="fa fa-fire" aria-hidden="true"></i>
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                            <i class="fa fa-bell-o" aria-hidden="true"></i>
+                            <i class="fa fa-bolt" aria-hidden="true"></i>
+                            <i class="fa fa-flask" aria-hidden="true"></i>
+                            <i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+                            <i class="fa fa-html5" aria-hidden="true"></i>
+                            <i class="fa fa-gamepad" aria-hidden="true"></i>
+                            <i class="fa fa-globe" aria-hidden="true"></i>
+                        </div>
+                        <div class="clearfix"></div>
+                        <!------get the code----------->
+                        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                            <div class="panel panel-default">
+                                <div class="panel-heading" role="tab" id="headingOne">
+                                    <h4 class="panel-title">
+                                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                            <i class="more-less glyphicon glyphicon-plus"></i>
+                                            Get the code
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                    <div class="panel-body">
+                                        <p>[gdlr_icon type="icon-fire" size="60px" color="#91d549"]<br>
+                                            [gdlr_icon type="icon-instagram" size="60px" color="#49d5c0"]<br>
+                                            [gdlr_icon type="icon-figther-jet" size="60px" color="#d55949"]<br>
+                                            [gdlr_icon type="icon-bell" size="60px" color="#4984d5"]<br>
+                                            [gdlr_icon type="icon-bolt" size="60px" color="#ea803a"]<br>
+                                            [gdlr_icon type="icon-beaker" size="60px" color="#b649d5"]<br>
+                                            [gdlr_icon type="icon-hand-right" size="60px" color="#91d549"]<br>
+                                            [gdlr_icon type="icon-html5" size="60px" color="#e5427b"]<br>
+                                            [gdlr_icon type="icon-gamepad" size="60px" color="#d55949"]<br>
+                                            [gdlr_icon type="icon-globe" size="60px" color="#4984d5"]</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
+<script>
+    function toggleIcon(e) {
+        $(e.target)
+                .prev('.panel-heading')
+                .find(".more-less")
+                .toggleClass('glyphicon-plus glyphicon-minus');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+</script>
